76c5fd4dcff96ba97a1ac33a6ef0cb2e64d828a6
kiranpranay <pranaybannu12@gmail.com>
2023-06-14 00:57:18 +0530

    Add gpt model model parameter
    By defualt, the model would be gpt-4.
    But, the user can specify, the model they want by
    
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ app = typer.Typer()
 @app.command()
 def chat(
     project_path: str = typer.Argument(None, help="path"),
+    model: str = typer.Argument("gpt-4", help="model name"),
     run_prefix: str = typer.Option("", help="run prefix, if you want to run multiple variants of the same project and later compare them"),
-    model: str = "gpt-4",
     temperature: float = 0.1,
 ):
 
