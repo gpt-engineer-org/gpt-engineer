e5864d40798ff58155e3a15c586c6b9913c39a77
Aayushman Jain <94888896+AayushTech@users.noreply.github.com>
2023-06-14 17:53:59 +0530

    Update ai.py
    Just adding the comments in the code for people to understand what exactly is going on

--- ai.py
+++ ai.py
@@ -1,28 +1,27 @@
 
 import openai
 
-
 class AI:
     def __init__(self, **kwargs):
         self.kwargs = kwargs
 
     def start(self, system, user):
         messages = [
-                {"role": "system", "content": system},
-                {"role": "user", "content": user},
-            ]
+            {"role": "system", "content": system},  # System message
+            {"role": "user", "content": user},      # User message
+        ]
 
         return self.next(messages)
 
     def fsystem(self, msg):
-        return {"role": "system", "content": msg}
+        return {"role": "system", "content": msg}  # Construct system message object
     
     def fuser(self, msg):
-        return {"role": "user", "content": msg}
+        return {"role": "user", "content": msg}    # Construct user message object
 
     def next(self, messages: list[dict[str, str]], prompt=None):
         if prompt:
-            messages = messages + [{"role": "user", "content": prompt}]
+            messages = messages + [{"role": "user", "content": prompt}]  # Add prompt as user message
 
         response = openai.ChatCompletion.create(
             messages=messages,
@@ -36,4 +35,11 @@ class AI:
             msg = delta.get('content', '')
             print(msg, end="")
             chat.append(msg)
-        return messages + [{"role": "assistant", "content": "".join(chat)}]
+        return messages + [{"role": "assistant", "content": "".join(chat)}]  # Add assistant's message to chat history
+
+# Create an instance of the AI class
+ai = AI()
+
+# Example usage:
+# Start a conversation with a system message and a user message
+ai.start("Hello, how can I assist you?", "Can you provide information about your products?")
