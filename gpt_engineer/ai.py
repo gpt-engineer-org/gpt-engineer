ea3b2e7e0589d54a7e31f7de5183ef411154e240
Benard Mutua <bmutua350@gmail.com>
2023-06-16 15:22:39 +0300

    Update ai.py
    update on next function:  List[Dict[str, str]]

--- gpt_engineer/ai.py
+++ gpt_engineer/ai.py
@@ -1,19 +1,21 @@
 import openai
+from typing import List, Dict, Union
 
 
 class AI:
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: Union[str, int, bool]):
         self.kwargs = kwargs
 
         try:
+            # Check if gpt-4 model is available
             openai.Model.retrieve("gpt-4")
         except openai.error.InvalidRequestError:
-            print("Model gpt-4 not available for provided api key reverting "
-                  "to gpt-3.5.turbo. Sign up for the gpt-4 wait list here: "
-                  "https://openai.com/waitlist/gpt-4-api")
+            # If gpt-4 is not available, fallback to gpt-3.5-turbo
+            print("Model gpt-4 not available for the provided API key. Reverting to gpt-3.5-turbo.")
+            print("Sign up for the gpt-4 waitlist here: https://openai.com/waitlist/gpt-4-api")
             self.kwargs['model'] = "gpt-3.5-turbo"
 
-    def start(self, system, user):
+    def start(self, system: str, user: str) -> List[Dict[str, str]]:
         messages = [
             {"role": "system", "content": system},
             {"role": "user", "content": user},
@@ -21,13 +23,13 @@ class AI:
 
         return self.next(messages)
 
-    def fsystem(self, msg):
+    def fsystem(self, msg: str) -> Dict[str, str]:
         return {"role": "system", "content": msg}
 
-    def fuser(self, msg):
+    def fuser(self, msg: str) -> Dict[str, str]:
         return {"role": "user", "content": msg}
 
-    def next(self, messages: list[dict[str, str]], prompt=None):
+    def next(self, messages: List[Dict[str, str]], prompt: str = None) -> List[Dict[str, str]]:
         if prompt:
             messages = messages + [{"role": "user", "content": prompt}]
 
@@ -37,8 +39,9 @@ class AI:
 
         chat = []
         for chunk in response:
-            delta = chunk["choices"][0]["delta"]
-            msg = delta.get("content", "")
+            delta = chunk['choices'][0]['delta']
+            msg = delta.get('content', '')
             print(msg, end="")
             chat.append(msg)
+
         return messages + [{"role": "assistant", "content": "".join(chat)}]
