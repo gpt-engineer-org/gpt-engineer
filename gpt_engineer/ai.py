d4680587596eb305ab1db8f68e3c99e80280b89c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-06-17 22:52:05 +0200

    Use gpt-3.5-turbo-16k-0613 as fallback model

--- gpt_engineer/ai.py
+++ gpt_engineer/ai.py
@@ -11,7 +11,7 @@ class AI:
             print("Model gpt-4 not available for provided api key reverting "
                   "to gpt-3.5-turbo-16k. Sign up for the gpt-4 wait list here: "
                   "https://openai.com/waitlist/gpt-4-api")
-            self.kwargs['model'] = "gpt-3.5-turbo-16k"
+            self.kwargs['model'] = "gpt-3.5-turbo-16k-0613"
 
     def start(self, system, user):
         messages = [
