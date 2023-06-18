613aab8ef731b733c17a51e6c4c94cf471b7537f
David Cameron <dave.j.cameron@gmail.com>
2023-06-18 19:07:58 -0400

    Fix square bracket file name issue

--- gpt_engineer/chat_to_files.py
+++ gpt_engineer/chat_to_files.py
@@ -11,6 +11,9 @@ def parse_chat(chat):  # -> List[Tuple[str, str]]:
         # Strip the filename of any non-allowed characters and convert / to \
         path = re.sub(r'[<>"|?*]', "", match.group(1))
 
+        # Remove leading and trailing brackets
+        path = re.sub(r"^\[(.*)\]$", r"\1", path)
+
         # Get the code
         code = match.group(2)
 
