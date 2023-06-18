f62a0eefb3aebb6c635ee3492a7d4c84826766d3
Philipp Kohler <39829783+Snoup97@users.noreply.github.com>
2023-06-18 09:42:14 +0000

    new line between blocks in test

--- tests/test_parse_chat.py
+++ tests/test_parse_chat.py
@@ -2,8 +2,7 @@ import pytest
 from gpt_engineer.chat_to_files import parse_chat
 
 def test_parse_chat_with_valid_messages():
-    chat = "```path1.py\nline1\nline2```" \
-           "```path2.css\nline3\nline4```"
+    chat = "```path1.py\nline1\nline2```\n```path2.css\nline3\nline4```"
     expected_output = [('path1.py', 'line1\nline2'), ('path2.css', 'line3\nline4')]
 
     output = parse_chat(chat)
