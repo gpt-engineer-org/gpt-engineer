e53d6f467f63c6cf6dec10c7f2b380084ea1dbb1
Philipp Kohler <39829783+Snoup97@users.noreply.github.com>
2023-06-23 10:17:52 +0000

    fix typos

--- gpt_engineer/steps.py
+++ gpt_engineer/steps.py
@@ -181,7 +181,7 @@ def gen_entrypoint(ai, dbs):
             "From this you will answer with code blocks that includes all the necessary "
             "unix terminal commands to "
             "a) install dependencies "
-            "b) run all necessary parts of the codebase (in parallell if necessary).\n"
+            "b) run all necessary parts of the codebase (in parallel if necessary).\n"
             "Do not install globally. Do not use sudo.\n"
             "Do not explain the code, just give the commands.\n"
             "Do not use placeholders, use example values (like . for a folder argument) "
