fe59374b438edb58184ffbf6b99c61c18634be66
kafka <bishalkharel0000@gmail.com>
2023-06-27 20:16:15 +0545

    refactor: refactored

--- scripts/rerun_edited_message_logs.py
+++ scripts/rerun_edited_message_logs.py
@@ -1,7 +1,6 @@
 import json
 import pathlib
-
-from typing import Union
+from typing import Optional, Union
 
 import typer
 
@@ -10,18 +9,14 @@ from gpt_engineer.chat_to_files import to_files
 
 app = typer.Typer()
 
-
 @app.command()
 def main(
     messages_path: str,
-    out_path: Union[str, None] = None,
     model: str = "gpt-4",
     temperature: float = 0.1,
+    out_path: Optional[str] = None,
 ):
-    ai = AI(
-        model=model,
-        temperature=temperature,
-    )
+    ai = AI(model=model, temperature=temperature)
 
     with open(messages_path) as f:
         messages = json.load(f)
@@ -32,7 +27,6 @@ def main(
         to_files(messages[-1]["content"], out_path)
         with open(pathlib.Path(out_path) / "all_output.txt", "w") as f:
             json.dump(messages[-1]["content"], f)
-
 
 if __name__ == "__main__":
     app()
