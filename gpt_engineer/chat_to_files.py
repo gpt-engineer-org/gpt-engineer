d4ab42af88df9fea6c5c70d68f2effd4e8284c1a
H <holden.robbins@gmail.com>
2023-06-30 21:05:37 +0000

    Added ':' to characters to remove from end of file path

--- gpt_engineer/chat_to_files.py
+++ gpt_engineer/chat_to_files.py
@@ -9,7 +9,7 @@ def parse_chat(chat):  # -> List[Tuple[str, str]]:
     files = []
     for match in matches:
         # Strip the filename of any non-allowed characters and convert / to \
-        path = re.sub(r'[<>"|?*]', "", match.group(1))
+        path = re.sub(r'[\:<>"|?*]', "", match.group(1))
 
         # Remove leading and trailing brackets
         path = re.sub(r"^\[(.*)\]$", r"\1", path)
@@ -18,7 +18,7 @@ def parse_chat(chat):  # -> List[Tuple[str, str]]:
         path = re.sub(r"^`(.*)`$", r"\1", path)
 
         # Remove trailing ]
-        path = re.sub(r"\]$", "", path)
+        path = re.sub(r"[\]\:]$", "", path)
 
         # Get the code
         code = match.group(2)
