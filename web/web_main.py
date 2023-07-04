a39507e6438eed1ffc936451d79ebbd3ee836268
SumitKumarDev10 <sumitkb23@gmail.com>
2023-07-04 18:25:59 +0530

    Update web_main.py
    Added initial contents of web_main.py

---
+++ web/web_main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
