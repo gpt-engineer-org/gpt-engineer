8980a6bc9828ea5a80eec570f79574138e8a787d
SumitKumarDev10 <sumitkb23@gmail.com>
2023-07-04 19:16:29 +0530

    Update web_main.py
    Added aboutus route

--- web/web_main.py
+++ web/web_main.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def home_page():
     return render_template('index.html')
+
+@app.route('/aboutus')
+def aboutus_page():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
