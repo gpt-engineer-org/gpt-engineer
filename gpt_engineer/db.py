8888de2ed9f55d94c471d9056baa7bf049265909
jesse <jessemhan@proton.me>
2023-07-16 22:12:35 -0700

    update

--- gpt_engineer/db.py
+++ gpt_engineer/db.py
@@ -3,23 +3,26 @@ import shutil
 
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Any, Dict, Optional
 
 
 # This class represents a simple database that stores its data as files in a directory.
 class DB:
     """A simple key-value store, where keys are filenames and values are file contents."""
 
-    def __init__(self, path):
+    def __init__(self, path, in_memory_dict: Optional[Dict[Any, Any]] = None):
         self.path = Path(path).absolute()
 
         self.path.mkdir(parents=True, exist_ok=True)
+        self.in_memory_dict = in_memory_dict
 
     def __contains__(self, key):
         return (self.path / key).is_file()
 
     def __getitem__(self, key):
+        if self.in_memory_dict is not None:
+            return self.in_memory_dict.__getitem__(str((self.path / key).absolute()))
         full_path = self.path / key
-
         if not full_path.is_file():
             raise KeyError(f"File '{key}' could not be found in '{self.path}'")
         with full_path.open("r", encoding="utf-8") as f:
@@ -32,6 +35,8 @@ class DB:
             return default
 
     def __setitem__(self, key, val):
+        if self.in_memory_dict is not None:
+            return self.in_memory_dict.__setitem__(str((self.path / key).absolute()), val)
         full_path = self.path / key
         full_path.parent.mkdir(parents=True, exist_ok=True)
 
