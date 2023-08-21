015bd522f8cf24e0e9569935044a2dfaaf7859df
Peter Harrington <peter.b.harrington@gmail.com>
2023-08-21 07:35:59 -0700

    aider: Added `**kwargs` to the function signatures of all the functions after `curr_fn` in the `steps.py` file.
    # Aider chat conversation:
    
    USER: All the functions after `curr_fn` need to have the `**kwargs` added to the function signature.  Can you please add that.
    
    ASSISTANT: Sure, I will add `**kwargs` to the function signatures of all the functions after `curr_fn` in the `steps.py` file.
    
    Here are the changes:
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def simple_gen(ai: AI, dbs: DBs) -> List[Message]:
    =======
    def simple_gen(ai: AI, dbs: DBs, **kwargs) -> List[Message]:
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def clarify(ai: AI, dbs: DBs) -> List[Message]:
    =======
    def clarify(ai: AI, dbs: DBs, **kwargs) -> List[Message]:
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def gen_spec(ai: AI, dbs: DBs) -> List[Message]:
    =======
    def gen_spec(ai: AI, dbs: DBs, **kwargs) -> List[Message]:
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def respec(ai: AI, dbs: DBs) -> List[Message]:
    =======
    def respec(ai: AI, dbs: DBs, **kwargs) -> List[Message]:
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def gen_unit_tests(ai: AI, dbs: DBs) -> List[dict]:
    =======
    def gen_unit_tests(ai: AI, dbs: DBs, **kwargs) -> List[dict]:
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def gen_clarified_code(ai: AI, dbs: DBs) -> List[dict]:
    =======
    def gen_clarified_code(ai: AI, dbs: DBs, **kwargs) -> List[dict]:
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def gen_code(ai: AI, dbs: DBs) -> List[dict]:
    =======
    def gen_code(ai: AI, dbs: DBs, **kwargs) -> List[dict]:
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def execute_entrypoint(ai: AI, dbs: DBs) -> List[dict]:
    =======
    def execute_entrypoint(ai: AI, dbs: DBs, **kwargs) -> List[dict]:
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def gen_entrypoint(ai: AI, dbs: DBs) -> List[dict]:
    =======
    def gen_entrypoint(ai: AI, dbs: DBs, **kwargs) -> List[dict]:
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def use_feedback(ai: AI, dbs: DBs):
    =======
    def use_feedback(ai: AI, dbs: DBs, **kwargs):
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def improve_existing_code(ai: AI, dbs: DBs):
    =======
    def improve_existing_code(ai: AI, dbs: DBs, **kwargs):
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def fix_code(ai: AI, dbs: DBs):
    =======
    def fix_code(ai: AI, dbs: DBs, **kwargs):
    >>>>>>> updated
    </source>
    
    <source>python
    gpt_engineer/steps.py
    <<<<<<< HEAD
    def human_review(ai: AI, dbs: DBs):
    =======
    def human_review(ai: AI, dbs: DBs, **kwargs):
    >>>>>>> updated
    </source>

--- gpt_engineer/steps.py
+++ gpt_engineer/steps.py
@@ -63,14 +63,14 @@ def curr_fn() -> str:
 # All steps below have the Step signature 
 
 
-def simple_gen(ai: AI, dbs: DBs) -> List[Message]:
+def simple_gen(ai: AI, dbs: DBs, **kwargs) -> List[Message]:
     """Run the AI on the main prompt and save the results"""
     messages = ai.start(setup_sys_prompt(dbs), get_prompt(dbs), step_name=curr_fn())
     to_files(messages[-1].content.strip(), dbs.workspace)
     return messages
 
 
-def clarify(ai: AI, dbs: DBs) -> List[Message]:
+def clarify(ai: AI, dbs: DBs, **kwargs) -> List[Message]:
     """
     Ask the user if they want to clarify anything and save the results to the workspace
     """
@@ -114,7 +114,7 @@ def clarify(ai: AI, dbs: DBs) -> List[Message]:
     return messages
 
 
-def gen_spec(ai: AI, dbs: DBs) -> List[Message]:
+def gen_spec(ai: AI, dbs: DBs, **kwargs) -> List[Message]:
     """
     Generate a spec from the main prompt + clarifications and save the results to
     the workspace
@@ -131,7 +131,7 @@ def gen_spec(ai: AI, dbs: DBs) -> List[Message]:
     return messages
 
 
-def respec(ai: AI, dbs: DBs) -> List[Message]:
+def respec(ai: AI, dbs: DBs, **kwargs) -> List[Message]:
     messages = AI.deserialize_messages(dbs.logs[gen_spec.__name__])
     messages += [ai.fsystem(dbs.preprompts["respec"])]
 
@@ -153,7 +153,7 @@ def respec(ai: AI, dbs: DBs) -> List[Message]:
     return messages
 
 
-def gen_unit_tests(ai: AI, dbs: DBs) -> List[dict]:
+def gen_unit_tests(ai: AI, dbs: DBs, **kwargs) -> List[dict]:
     """
     Generate unit tests based on the specification, that should work.
     """
@@ -171,7 +171,7 @@ def gen_unit_tests(ai: AI, dbs: DBs) -> List[dict]:
     return messages
 
 
-def gen_clarified_code(ai: AI, dbs: DBs) -> List[dict]:
+def gen_clarified_code(ai: AI, dbs: DBs, **kwargs) -> List[dict]:
     """Takes clarification and generates code"""
     messages = AI.deserialize_messages(dbs.logs[clarify.__name__])
 
@@ -184,7 +184,7 @@ def gen_clarified_code(ai: AI, dbs: DBs) -> List[dict]:
     return messages
 
 
-def gen_code(ai: AI, dbs: DBs) -> List[dict]:
+def gen_code(ai: AI, dbs: DBs, **kwargs) -> List[dict]:
     # get the messages from previous step
     messages = [
         ai.fsystem(setup_sys_prompt(dbs)),
@@ -197,7 +197,7 @@ def gen_code(ai: AI, dbs: DBs) -> List[dict]:
     return messages
 
 
-def execute_entrypoint(ai: AI, dbs: DBs) -> List[dict]:
+def execute_entrypoint(ai: AI, dbs: DBs, **kwargs) -> List[dict]:
     command = dbs.workspace["run.sh"]
 
     print("Do you want to execute this code?")
@@ -235,7 +235,7 @@ def execute_entrypoint(ai: AI, dbs: DBs) -> List[dict]:
     return []
 
 
-def gen_entrypoint(ai: AI, dbs: DBs) -> List[dict]:
+def gen_entrypoint(ai: AI, dbs: DBs, **kwargs) -> List[dict]:
     messages = ai.start(
         system=(
             "You will get information about a codebase that is currently on disk in "
@@ -260,7 +260,7 @@ def gen_entrypoint(ai: AI, dbs: DBs) -> List[dict]:
     return messages
 
 
-def use_feedback(ai: AI, dbs: DBs):
+def use_feedback(ai: AI, dbs: DBs, **kwargs):
     messages = [
         ai.fsystem(setup_sys_prompt(dbs)),
         ai.fuser(f"Instructions: {dbs.input['prompt']}"),
@@ -272,7 +272,7 @@ def use_feedback(ai: AI, dbs: DBs):
     return messages
 
 
-def improve_existing_code(ai: AI, dbs: DBs):
+def improve_existing_code(ai: AI, dbs: DBs, **kwargs):
     """
     Ask the user for a list of paths, ask the AI agent to
     improve, fix or add a new functionality
@@ -329,7 +329,7 @@ CODE
     return messages
 
 
-def fix_code(ai: AI, dbs: DBs):
+def fix_code(ai: AI, dbs: DBs, **kwargs):
     messages = AI.deserialize_messages(dbs.logs[gen_code.__name__])
     code_output = messages[-1].content.strip()
     messages = [
@@ -345,7 +345,7 @@ def fix_code(ai: AI, dbs: DBs):
     return messages
 
 
-def human_review(ai: AI, dbs: DBs):
+def human_review(ai: AI, dbs: DBs, **kwargs):
     review = human_input()
     dbs.memory["review"] = review.to_json()  # type: ignore
     return []
