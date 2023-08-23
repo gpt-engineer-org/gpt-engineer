5fc893a5763f00397b9ed705887afe8c37e63cf6
Peter Harrington <peter.b.harrington@gmail.com>
2023-08-23 07:16:53 -0700

    aider: Added a function to eval tools which takes a dict named `tests` and does a switch on the key `type` from `tests`, with options for `assert_exists_in_source_code` and `run_code_class_has_property`, and a default option raising an exception.
    # Aider chat conversation:
    
    USER: add a function to eval tools which takes a dict named `tests` and does a switch on the key `type` from `tests`.  The possible options are: `assert_exists_in_source_code`, and `run_code_class_has_property`, there should also be a default option raising an excpetion.
    
    ASSISTANT: To add the requested function, we will need to modify the `scripts/eval_tools.py` file.
    
    Here are the steps we will follow:
    
    1. Define a new function named `run_tests` that takes a dictionary named `tests` as an argument.
    2. Inside this function, we will use a switch-like structure using Python's dictionary mapping to handle different `type` keys from the `tests` dictionary.
    3. For the `type` key `assert_exists_in_source_code`, we will call the `assert_exists_in_source_code` function.
    4. For the `type` key `run_code_class_has_property`, we will call a function `run_code_class_has_property`. This function is not defined yet, so we will add a placeholder function for now.
    5. If the `type` key is not recognized, we will raise an exception.
    
    Here is the *edit block* for the changes:
    
    ```python
    scripts/eval_tools.py
    <<<<<<< HEAD
    from pathlib import Path
    
    def assert_exists_in_source_code(fpath: Path, existing_code: str):
        source_body = open(fpath).read()
        return source_body.find(existing_code) > -1
    =======
    from pathlib import Path
    
    def assert_exists_in_source_code(fpath: Path, existing_code: str):
        source_body = open(fpath).read()
        return source_body.find(existing_code) > -1
    
    def run_code_class_has_property():
        # Placeholder function
        pass
    
    def run_tests(tests: dict):
        test_type = tests.get('type')
        if test_type == 'assert_exists_in_source_code':
            return assert_exists_in_source_code(tests.get('fpath'), tests.get('existing_code'))
        elif test_type == 'run_code_class_has_property':
            return run_code_class_has_property()
        else:
            raise Exception(f"Test type '{test_type}' is not recognized.")
    >>>>>>> updated
    ```
    
    Please note that the `run_code_class_has_property` function is a placeholder and does not perform any operation. You will need to replace it with the actual implementation.

--- scripts/eval_tools.py
+++ scripts/eval_tools.py
@@ -4,3 +4,18 @@ from pathlib import Path
 def assert_exists_in_source_code(fpath: Path, existing_code: str):
     source_body = open(fpath).read()
     return source_body.find(existing_code) > -1
+
+
+def run_code_class_has_property():
+    # Placeholder function
+    pass
+
+
+def run_tests(tests: dict):
+    test_type = tests.get('type')
+    if test_type == 'assert_exists_in_source_code':
+        return assert_exists_in_source_code(tests.get('fpath'), tests.get('existing_code'))
+    elif test_type == 'run_code_class_has_property':
+        return run_code_class_has_property()
+    else:
+        raise Exception(f"Test type '{test_type}' is not recognized.")
