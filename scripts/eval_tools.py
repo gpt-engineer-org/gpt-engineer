6c282048187142bdd4bbd2fc96bf1cdec2b0539d
Peter Harrington <peter.b.harrington@gmail.com>
2023-08-23 07:36:29 -0700

    not unpacking objects early

--- scripts/eval_tools.py
+++ scripts/eval_tools.py
@@ -15,16 +15,16 @@ def run_code_class_has_property(eval: dict) -> bool:
     if eval['language'] == 'python':
         return run_python_code(eval)
     else:
-        raise Exception(f"Language '{eval['language']}' is not supported.")
+        raise Exception(f"Language: {eval['language']} is not supported.")
 
 
-def check_evaluation_component(eval: dict):
+def check_evaluation_component(eval_d: dict):
     """Switch on evaluation components"""
-    test_type = eval.get('type')
+    test_type = eval_d.get('type')
     if test_type == 'assert_exists_in_source_code':
-        return assert_exists_in_source_code(eval.get('fpath'), eval.get('existing_code'))
+        return assert_exists_in_source_code(eval_d)
     elif test_type == 'run_code_class_has_property':
-        return run_code_class_has_property()
+        return run_code_class_has_property(eval_d)
     else:
         raise Exception(f"Test type '{test_type}' is not recognized.")
 
