bb4e673d3f33d5403257bca7f6ffd12f2385a623
Peter Harrington <peter.b.harrington@gmail.com>
2023-08-23 07:25:31 -0700

    rename machine generated functions

--- scripts/eval_tools.py
+++ scripts/eval_tools.py
@@ -1,21 +1,23 @@
 from pathlib import Path
 
 
-def assert_exists_in_source_code(fpath: Path, existing_code: str):
+def assert_exists_in_source_code(fpath: Path, existing_code: str) -> bool:
     source_body = open(fpath).read()
     return source_body.find(existing_code) > -1
 
 
-def run_code_class_has_property():
+def run_code_class_has_property(language: str, source_file: Path) -> bool:
     # Placeholder function
-    pass
+    return True
 
 
-def run_tests(tests: dict):
-    test_type = tests.get('type')
+def check_evaluation_component(eval: dict):
+    """Switch on evaluation components"""
+    test_type = eval.get('type')
     if test_type == 'assert_exists_in_source_code':
-        return assert_exists_in_source_code(tests.get('fpath'), tests.get('existing_code'))
+        return assert_exists_in_source_code(eval.get('fpath'), eval.get('existing_code'))
     elif test_type == 'run_code_class_has_property':
         return run_code_class_has_property()
     else:
         raise Exception(f"Test type '{test_type}' is not recognized.")
+
