030e81e28408f0594a4bcf5c1c3eda46beed304e
Peter Harrington <peter.b.harrington@gmail.com>
2023-08-24 08:00:28 -0700

    check evals working, it can test Python code functionally

--- scripts/evals_existing_code.py
+++ scripts/evals_existing_code.py
@@ -1,75 +1,67 @@
-import os
-import subprocess
-
 from pathlib import Path
 
 import yaml
 
-from gpt_engineer.chat_to_files import parse_chat
-from gpt_engineer.db import DB
+from eval_tools import check_evaluation_component
 
-EVAL_LIST_NAME = "evaluations"
-
-
-# these could be paramaters from one test
-SNAKE_EVAL_FOLDER = "projects/snake_game_eval"
-SNAKE_GAME_CODE_BLOB = "scripts/snake_game_files.txt"
-IMPROVE_CODE_PROMPT = "The grid is currently 10x10, change the grid to be 42x42."
+EVAL_LIST_NAME = "evaluations"  # the top level list in the YAML file
 
 
 def single_evaluate(eval_ob: dict) -> None:
     """Evaluates a single prompt."""
-    print(f"running evaluation: {eval_ob['name']}")
+    # print(f"running evaluation: {eval_ob['name']}")
 
-    # Step 1. Setup known project
-    # load the known files into the project
-    # the filex can be anywhere in the projects folder
+    # # Step 1. Setup known project
+    # # load the known files into the project
+    # # the files can be anywhere in the projects folder
 
-    workspace = DB(SNAKE_EVAL_FOLDER)
-    file_list_string = ""
-    code_base_abs = Path(os.getcwd()) / SNAKE_EVAL_FOLDER
+    # workspace = DB(eval_ob['project_root'])
+    # file_list_string = ""
+    # code_base_abs = Path(os.getcwd()) / eval_ob['project_root']
 
-    files = parse_chat(open(SNAKE_GAME_CODE_BLOB).read())
-    for file_name, file_content in files:
-        absolute_path = code_base_abs / file_name
-        print("creating: ", absolute_path)
-        workspace[absolute_path] = file_content
-        file_list_string += str(absolute_path) + "\n"
+    # files = parse_chat(open(eval_ob['SNAKE_GAME_CODE_BLOB']).read())
+    # for file_name, file_content in files:
+    #     absolute_path = code_base_abs / file_name
+    #     print("creating: ", absolute_path)
+    #     workspace[absolute_path] = file_content
+    #     file_list_string += str(absolute_path) + "\n"
 
-    # create file_list.txt (should be full paths)
-    workspace["file_list.txt"] = file_list_string
+    # # create file_list.txt (should be full paths)
+    # workspace["file_list.txt"] = file_list_string
 
-    # create the prompt
-    workspace["prompt"] = IMPROVE_CODE_PROMPT
+    # # create the prompt
+    # workspace["prompt"] = eval_ob['IMPROVE_CODE_PROMPT']
 
-    # Step 2.  run the project in improve code mode,
-    # make sure the flag -sf is set to skip feedback
+    # # Step 2.  run the project in improve code mode,
+    # # make sure the flag -sf is set to skip feedback
 
-    print(f"Running benchmark for {SNAKE_EVAL_FOLDER}")
+    # print(f"Modifying code for {eval_ob['project_root']}")
 
-    log_path = code_base_abs / "log.txt"
-    log_file = open(log_path, "w")
-    process = subprocess.Popen(
-        [
-            "python",
-            "-u",  # Unbuffered output
-            "-m",
-            "gpt_engineer.main",
-            SNAKE_EVAL_FOLDER,
-            "--steps",
-            "eval_improve_code",
-        ],
-        stdout=log_file,
-        stderr=log_file,
-        bufsize=0,
-    )
-    print(f"waiting for {eval_ob['name']} to finish.")
-    process.wait()
-    # we want to wait until it finishes.
+    # log_path = code_base_abs / "log.txt"
+    # log_file = open(log_path, "w")
+    # process = subprocess.Popen(
+    #     [
+    #         "python",
+    #         "-u",  # Unbuffered output
+    #         "-m",
+    #         "gpt_engineer.main",
+    #         eval_ob['project_root'],
+    #         "--steps",
+    #         "eval_improve_code",
+    #     ],
+    #     stdout=log_file,
+    #     stderr=log_file,
+    #     bufsize=0,
+    # )
+    # print(f"waiting for {eval_ob['name']} to finish.")
+    # process.wait()  # we want to wait until it finishes.
 
-    # TODO: check the code was properly changed.
-    # in grid.py check the code now looks like:
-    # def __init__(self, width=42, height=42):  # Updated grid size
+    # Step 3. run test of modified code, tests
+    print("running tests on modified code")
+    for test_case in eval_ob["expected_results"]:
+        print(f"checking: {test_case['type']}")
+        test_case["project_root"] = Path(eval_ob["project_root"])
+        check_evaluation_component(test_case)
 
 
 def load_evaluations_from_file(file_path):
