075aeee8e0fcc8c2cd40eeef4d934f43f5b89809
captivus <366332+captivus@users.noreply.github.com>
2023-10-02 21:22:59 -0500

    refactored core.domain

--- gpt_engineer/core/domain.py
+++ gpt_engineer/core/domain.py
@@ -1,3 +1,17 @@
+"""
+This module provides utilities and type annotations related to the steps workflow in GPT Engineer.
+
+Imports:
+    - `Callable`, `List`, and `TypeVar` from `typing` module, which are used for type hinting.
+    - `AI` class from the `gpt_engineer.core.ai` module.
+    - `DBs` class from the `gpt_engineer.core.db` module.
+
+Variables:
+    - `Step`: This is a generic type variable that represents a Callable or function type. The function
+      is expected to accept two parameters: an instance of `AI` and an instance of `DBs`. The function
+      is expected to return a list of dictionaries.
+"""
+
 from typing import Callable, List, TypeVar
 
 from gpt_engineer.core.ai import AI
