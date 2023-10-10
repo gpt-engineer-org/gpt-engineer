33070bf539dd4cfb1277015a2e98d1c175a83112
captivus <366332+captivus@users.noreply.github.com>
2023-10-09 20:37:40 -0500

    added installation test

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,7 +1,31 @@
 """
-Tests for successful import of the package and its modules
+Tests for successful import and installation of the package.
 """
-import traceback
+import subprocess
+import sys
+import venv
+import shutil
+
+# Test that the package can be installed via pip
+def test_installation():
+    # Create a virtual environment
+    venv_dir = "./venv_test_installation"
+    venv.create(venv_dir, with_pip=True)
+
+    # Use pip from the virtual environment directly
+    pip_executable = f"{venv_dir}/bin/pip"
+    if sys.platform == "win32":
+        pip_executable = f"{venv_dir}/Scripts/pip.exe"
+    
+    try:
+        result = subprocess.run(
+            [pip_executable, "install", "."], 
+            capture_output=True
+        )
+        assert result.returncode == 0, f"Install via pip failed: {result.stderr.decode()}"
+    finally:
+        # Cleanup
+        shutil.rmtree(venv_dir)
 
 
 # Test that the package can be imported
@@ -15,5 +39,4 @@ def test_import():
             db,
         )
     except ImportError as e:
-        # tb = traceback.format_exc()
         assert False, f"Failed to import {e.name}"
