37c6c108d1fb45dea9b74d80d07819f64fcdddb7
captivus <366332+captivus@users.noreply.github.com>
2023-10-09 20:47:06 -0500

    added cli execution test

--- tests/test_install.py
+++ tests/test_install.py
@@ -6,27 +6,22 @@ import sys
 import venv
 import shutil
 
+# Setup the test environment
+VENV_DIR = "./venv_test_installation"
+venv.create(VENV_DIR, with_pip=True)
+
 # Test that the package can be installed via pip
 def test_installation():
-    # Create a virtual environment
-    venv_dir = "./venv_test_installation"
-    venv.create(venv_dir, with_pip=True)
-
     # Use pip from the virtual environment directly
-    pip_executable = f"{venv_dir}/bin/pip"
+    pip_executable = f"{VENV_DIR}/bin/pip"
     if sys.platform == "win32":
-        pip_executable = f"{venv_dir}/Scripts/pip.exe"
+        pip_executable = f"{VENV_DIR}/Scripts/pip.exe"
     
-    try:
-        result = subprocess.run(
-            [pip_executable, "install", "."], 
-            capture_output=True
-        )
-        assert result.returncode == 0, f"Install via pip failed: {result.stderr.decode()}"
-    finally:
-        # Cleanup
-        shutil.rmtree(venv_dir)
-
+    result = subprocess.run(
+        [pip_executable, "install", "."], 
+        capture_output=True
+    )
+    assert result.returncode == 0, f"Install via pip failed: {result.stderr.decode()}"
 
 # Test that the package can be imported
 def test_import():
@@ -40,3 +35,25 @@ def test_import():
         )
     except ImportError as e:
         assert False, f"Failed to import {e.name}"
+
+# Test that the CLI command works
+def test_cli_execution():
+    # This assumes that after installation, `gpt-engineer` command should work.
+    result = subprocess.run(
+        args=["gpt-engineer", "--help"], 
+        capture_output=True, 
+        text=True
+        )
+    assert result.returncode == 0, f"gpt-engineer command failed with message: {result.stderr}"
+
+# Cleanup the test environment
+def test_cleanup():
+    shutil.rmtree(VENV_DIR)
+
+
+# Run the tests using pytest
+if __name__ == "__main__":
+    test_installation()
+    test_import()
+    test_cli_execution()
+    test_cleanup()
