b0c287d59e66fe5f5da7d3552719100d5e8c604b
captivus <366332+captivus@users.noreply.github.com>
2023-10-09 21:05:21 -0500

    ran black

--- tests/test_install.py
+++ tests/test_install.py
@@ -10,18 +10,17 @@ import shutil
 VENV_DIR = "./venv_test_installation"
 venv.create(VENV_DIR, with_pip=True)
 
+
 # Test that the package can be installed via pip
 def test_installation():
     # Use pip from the virtual environment directly
     pip_executable = f"{VENV_DIR}/bin/pip"
     if sys.platform == "win32":
         pip_executable = f"{VENV_DIR}/Scripts/pip.exe"
-    
-    result = subprocess.run(
-        [pip_executable, "install", "."], 
-        capture_output=True
-    )
+
+    result = subprocess.run([pip_executable, "install", "."], capture_output=True)
     assert result.returncode == 0, f"Install via pip failed: {result.stderr.decode()}"
+
 
 # Test that the package can be imported
 def test_import():
@@ -36,15 +35,17 @@ def test_import():
     except ImportError as e:
         assert False, f"Failed to import {e.name}"
 
+
 # Test that the CLI command works
 def test_cli_execution():
     # This assumes that after installation, `gpt-engineer` command should work.
     result = subprocess.run(
-        args=["gpt-engineer", "--help"], 
-        capture_output=True, 
-        text=True
-        )
-    assert result.returncode == 0, f"gpt-engineer command failed with message: {result.stderr}"
+        args=["gpt-engineer", "--help"], capture_output=True, text=True
+    )
+    assert (
+        result.returncode == 0
+    ), f"gpt-engineer command failed with message: {result.stderr}"
+
 
 # Cleanup the test environment
 def test_cleanup():
