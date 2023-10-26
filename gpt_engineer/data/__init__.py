93f76c8eb55b70d77102c1203bcfae9a834512ba
Theo McCabe <theomccabe@mac.com>
2023-10-26 18:26:42 +0100

    data module cleanup

--- gpt_engineer/data/__init__.py
+++ gpt_engineer/data/__init__.py
@@ -1,16 +1,9 @@
 """
-gpt_engineer.core
+gpt_engineer.data
 -----------------
 
-The core package for the GPT Engineer project, providing essential modules
-and functionalities that form the foundation of the application.
-
 Modules:
-    - ai: Contains interfaces to the OpenAI GPT models.
-    - domain: Contains type annotations related to the steps workflow in GPT Engineer.
-    - chat_to_files: Provides utilities for converting chat model outputs to files.
-    - steps: Primary workflow definition & configuration for GPT Engineer.
-    - db: Provides file system operations for GPT Engineer projects.
+    - code_vector_repository
+    - document_chunker
 
-For more specific details, refer to the docstrings within each module.
 """
