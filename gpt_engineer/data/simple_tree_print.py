5441fda146f15fbea9381db2d002e1b2dc87bc77
TheoMcCabe <thoemccabe@mac.com>
2023-10-28 19:44:13 +0100

    add tests

--- gpt_engineer/data/simple_tree_print.py
+++
@@ -1,8 +0,0 @@
-from file_repository import FileRepository
-
-
-workspace = FileRepository("../example-big")
-print(workspace.to_path_list_string(True))
-print(workspace.to_path_xml_string(True))
-print(workspace.to_path_list_string(False))
-print(workspace.to_path_xml_string(False))
