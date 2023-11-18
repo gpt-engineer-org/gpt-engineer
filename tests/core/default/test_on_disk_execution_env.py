8ce445e48c9bdd68dcb92052098ef764d8abeaa1
Axel Theorell <axel.theorell@gmail.com>
2023-11-18 20:08:35 +0100

    Tests made for OnDiskExecutionEnv

---
+++ tests/core/default/test_on_disk_execution_env.py
@@ -0,0 +1,74 @@
+import unittest
+from gpt_engineer.core.default.on_disk_execution_env import OnDiskExecutionEnv
+from gpt_engineer.core.default.paths import ENTRYPOINT_FILE
+import tempfile
+from unittest.mock import patch, MagicMock
+
+
+class TestOnDiskExecutionEnv(unittest.TestCase):
+
+    def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.env = OnDiskExecutionEnv(self.temp_dir.name)
+
+    def tearDown(self):
+        self.temp_dir.cleanup()
+
+    def test_successful_execution(self):
+        entrypoint_content = """
+        python -m venv venv
+        source venv/bin/activate
+        python script.py
+        """
+        code = {
+            ENTRYPOINT_FILE: entrypoint_content,
+            "script.py": "print('This is a test script')"
+        }
+        with patch('subprocess.Popen') as mock_popen:
+            mock_popen.return_value.wait.return_value = 0
+            process = self.env.execute_program(code)
+            self.assertIsNotNone(process)
+            mock_popen.assert_called_once()
+
+    def test_missing_entrypoint(self):
+        code = {
+            "script.py": "print('This is a test script')"
+        }
+        with self.assertRaises(FileNotFoundError):
+            self.env.execute_program(code)
+
+    def test_keyboard_interrupt_handling(self):
+        entrypoint_content = """
+        python script.py
+        """
+        code = {
+            ENTRYPOINT_FILE: entrypoint_content,
+            "script.py": "print('This is a test script')"
+        }
+        with patch('subprocess.Popen') as mock_popen:
+            mock_process = MagicMock()
+            mock_process.wait.side_effect = KeyboardInterrupt
+            mock_popen.return_value = mock_process
+            self.env.execute_program(code)
+            mock_process.kill.assert_called_once()
+
+    def test_execution_with_output(self):
+        entrypoint_content = """
+        python script.py
+        """
+        code = {
+            ENTRYPOINT_FILE: entrypoint_content,
+            "script.py": "import sys; print('Out'); sys.stderr.write('Error')"
+        }
+        with patch('subprocess.Popen') as mock_popen:
+            process = MagicMock()
+            process.wait.return_value = 0
+            process.communicate.return_value = (b'Out\n', b'Error\n')
+            mock_popen.return_value = process
+            process = self.env.execute_program(code)
+            stdout, stderr = process.communicate()
+            self.assertEqual(stdout, b'Out\n')
+            self.assertEqual(stderr, b'Error\n')
+
+if __name__ == '__main__':
+    unittest.main()
