91bdc7d62404c73b101daa450063de5cd6f36ad2
Axel Theorell <axel.theorell@gmail.com>
2023-11-21 15:38:19 +0100

    Iterative refinement of edits (not working great still)

--- gpt_engineer/core/default/steps.py
+++ gpt_engineer/core/default/steps.py
@@ -1,6 +1,6 @@
 from gpt_engineer.core.code import Code
 from gpt_engineer.core.ai import AI
-from gpt_engineer.core.chat_to_files import parse_chat, overwrite_code_with_edits
+from gpt_engineer.core.chat_to_files import parse_chat, overwrite_code_with_edits, parse_edits
 from gpt_engineer.core.default.paths import (
     ENTRYPOINT_FILE,
     CODE_GEN_LOG_FILE,
@@ -8,11 +8,12 @@ from gpt_engineer.core.default.paths import (
     IMPROVE_LOG_FILE,
     PREPROMPTS_PATH,
 )
+from gpt_engineer.core.default.constants import MAX_EDIT_REFINEMENT_STEPS
 from gpt_engineer.core.default.on_disk_repository import OnDiskRepository
 from gpt_engineer.core.base_repository import BaseRepository
 from gpt_engineer.core.base_execution_env import BaseExecutionEnv
 
-from typing import Union, MutableMapping
+from typing import Union, MutableMapping, List
 from pathlib import Path
 from langchain.schema import HumanMessage, SystemMessage
 import inspect
@@ -191,6 +192,20 @@ def setup_sys_prompt_existing_code(preprompts: MutableMapping[Union[str, Path], 
     )
 
 
+def incorrect_edit(code: Code, chat: str) -> List[str,]:
+    problems = list()
+    try:
+        edits = parse_edits(chat)
+    except ValueError as problem:
+        print("Not possible to parse chat to edits")
+        problems.append(str(problem))
+    for edit in edits:
+        if not edit.before in code[edit.filename]:
+            problems.append("This section, assigned to be exchanged for an edit block, does not have an exact match in the code: " + edit.before + "\nThis is often a result of placeholders, such as ... or references to 'existing code' or 'rest of function' etc, which cannot be used the HEAD part of the edit blocks. Also, to get a match, all comments, including long doc strings may have to be reproduced in the patch HEAD")
+    return problems
+
+
+
 def improve(ai: AI, prompt: str, code: Code, memory: BaseRepository, preprompts_path: Union[str, Path]) -> Code:
     """
     Improves existing code based on user input using an AI model.
@@ -217,9 +232,17 @@ def improve(ai: AI, prompt: str, code: Code, memory: BaseRepository, preprompts_
 
     messages.append(HumanMessage(content=f"Request: {prompt}"))
 
-    messages = ai.next(messages, step_name=curr_fn())
 
-    chat = messages[-1].content.strip()
+    problems = [""]
+    # check edit correctness
+    edit_refinements = 0
+    while len(problems) > 0 and edit_refinements <= MAX_EDIT_REFINEMENT_STEPS:
+        messages = ai.next(messages, step_name=curr_fn())
+        chat = messages[-1].content.strip()
+        problems = incorrect_edit(code, chat)
+        if len(problems) > 0:
+            messages.append(HumanMessage(content=f"Some previously produced edits were not on the requested format, or the HEAD part was not found in the code. Details: " + "\n".join(problems) + "\n Please provide ALL the edits again, making sure that the failing ones are now on the correct format and can be found in the code. Make sure to not repeat past mistakes. \n"))
+        edit_refinements += 1
 
     overwrite_code_with_edits(chat, code)
 
