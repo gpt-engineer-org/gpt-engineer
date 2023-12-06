ff31e02af6e91371f18ca51651c2d41fe5826fac
Marten <marten.wiman@gmail.com>
2023-12-06 10:54:48 +0100

    Remove excess indentation in files included in context.

--- gpt_engineer/core/chat_to_files.py
+++ gpt_engineer/core/chat_to_files.py
@@ -175,11 +175,11 @@ def format_file_to_input(file_name: str, file_content: str) -> str:
         The formatted file string.
     """
     file_str = f"""
-    {file_name}
-    ```
-    {file_content}
-    ```
-    """
+{file_name}
+```
+{file_content}
+```
+"""
     return file_str
 
 
