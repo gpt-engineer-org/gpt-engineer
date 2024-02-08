33cd57f93dc9b220fcd3b6f1b276aa153a6e6c06
Axel Theorell <axel.theorell@gmail.com>
2024-02-08 20:08:03 +0100

    committing random test stuff for improve PR

--- projects/example-improve/controller.py
+++ projects/example-improve/controller.py
@@ -7,11 +7,15 @@ class Controller:
         self.view = view
 
     def handle_input(self):
-        if keyboard.is_pressed("up"):
-            self.game.move("up")
-        elif keyboard.is_pressed("down"):
+        if keyboard.is_pressed("up") and not hasattr(self, "last_key_pressed"):
             self.game.move("down")
-        elif keyboard.is_pressed("left"):
-            self.game.move("left")
-        elif keyboard.is_pressed("right"):
+            self.last_key_pressed = "up"
+        elif hasattr(self, "last_key_pressed") and self.last_key_pressed == "up":
             self.game.move("right")
+            del self.last_key_pressed
+        elif keyboard.is_pressed("down"):
+            self.game.move("up")
+        elif keyboard.is_pressed("left"):
+            self.game.move("right")
+        elif keyboard.is_pressed("right"):
+            self.game.move("left")
