f264769a46274d2d65c4669d213d04876bede0f6
Talion <yzheng10@unb.ca>
2024-02-24 18:25:54 -0400

    refactor diff class

--- gpt_engineer/core/diff.py
+++ gpt_engineer/core/diff.py
@@ -129,6 +129,69 @@ class Hunk:
         else:
             pass
 
+    def find_start_line(self, lines_dict: dict, problems: list) -> bool:
+        # now find the true starting line compare to all lines and see how many matches we get
+        # ToDo handle the case where the start line is 0 or 1 characters separately
+        # handle the case where the start line is an add
+        if self.lines[0][0] == ADD:
+            start_line = None
+            # find the first line that is not an add
+            for index, line in enumerate(self.lines):
+                if line[0] != ADD:
+                    for line_number, line_content in lines_dict.items():
+                        # if the line is similar to a non-blank line in line_dict, we can pick the line prior to it
+                        if is_similar(line[1], line_content) and line[1] != "":
+                            start_line = line_number - 1
+                            break
+                    # if the start line is not found, this should be a comment from LLM
+                    if start_line is None:
+                        self.relabel_line(index, ADD)
+                        continue
+                    else:
+                        # the line prior to the start line is found now we insert it to the first place as the start line
+                        self.start_line_pre_edit = start_line
+                        retain_line = lines_dict.get(start_line, "")
+                        if retain_line:
+                            self.add_retained_line(lines_dict[start_line], 0)
+                            return self.validate_and_correct(lines_dict, problems)
+                        else:
+                            problems.append(
+                                f"In {self.hunk_to_string()}:The starting line of the diff {self.hunk_to_string()} does not exist in the code"
+                            )
+                            return False
+        pot_start_lines = {
+            key: is_similar(self.lines[0][1], line) for key, line in lines_dict.items()
+        }
+        sum_of_matches = sum(pot_start_lines.values())
+        if sum_of_matches == 0:
+            # before we go any further, we should check if it's a comment from LLM
+            if self.lines[0][1].count("#") > 0:
+                # if it is, we can mark it as an ADD lines
+                self.relabel_line(0, ADD)
+                # and restart the validation at the next line
+                return self.validate_and_correct(lines_dict, problems)
+
+            else:
+                problems.append(
+                    f"In {self.hunk_to_string()}:The starting line of the diff {self.hunk_to_string()} does not exist in the code"
+                )
+                return False
+        elif sum_of_matches == 1:
+            start_ind = list(pot_start_lines.keys())[
+                list(pot_start_lines.values()).index(True)
+            ]  # lines are one indexed
+        else:
+            logging.warning("multiple candidates for starting index")
+            # ToDo handle all the cases better again here. Smartest choice is that, for each candidate check match to the next line etc (recursively)
+            start_ind = list(pot_start_lines.keys())[
+                list(pot_start_lines.values()).index(True)
+            ]
+        self.start_line_pre_edit = start_ind
+
+        # This should now be fulfilled by default
+        assert is_similar(self.lines[0][1], lines_dict[self.start_line_pre_edit])
+        return True
+
     def validate_and_correct(
         self,
         lines_dict: dict,
@@ -143,66 +206,8 @@ class Hunk:
         start_true = self.check_start_line(lines_dict)
 
         if not start_true:
-            # now find the true starting line compare to all lines and see how many matches we get
-            # ToDo handle the case where the start line is 0 or 1 characters separately
-            # handle the case where the start line is an add
-            if self.lines[0][0] == ADD:
-                start_line = None
-                # find the first line that is not an add
-                for index, line in enumerate(self.lines):
-                    if line[0] != ADD:
-                        for line_number, line_content in lines_dict.items():
-                            # if the line is similar to a non-blank line in line_dict, we can pick the line prior to it
-                            if is_similar(line[1], line_content) and line[1] != "":
-                                start_line = line_number - 1
-                                break
-                        # if the start line is not found, this should be a comment from LLM
-                        if start_line is None:
-                            self.relabel_line(index, ADD)
-                            continue
-                        else:
-                            # the line prior to the start line is found now we insert it to the first place as the start line
-                            self.start_line_pre_edit = start_line
-                            retain_line = lines_dict.get(start_line, "")
-                            if retain_line:
-                                self.add_retained_line(lines_dict[start_line], 0)
-                                return self.validate_and_correct(lines_dict, problems)
-                            else:
-                                problems.append(
-                                    f"In {self.hunk_to_string()}:The starting line of the diff {self.hunk_to_string()} does not exist in the code"
-                                )
-                                return False
-            pot_start_lines = {
-                key: is_similar(self.lines[0][1], line)
-                for key, line in lines_dict.items()
-            }
-            sum_of_matches = sum(pot_start_lines.values())
-            if sum_of_matches == 0:
-                # before we go any further, we should check if it's a comment from LLM
-                if self.lines[0][1].count("#") > 0:
-                    # if it is, we can mark it as an ADD lines
-                    self.relabel_line(0, ADD)
-                    # and restart the validation at the next line
-                    return self.validate_and_correct(lines_dict, problems)
-
-                else:
-                    problems.append(
-                        f"In {self.hunk_to_string()}:The starting line of the diff {self.hunk_to_string()} does not exist in the code"
-                    )
-                    return False
-            elif sum_of_matches == 1:
-                start_ind = list(pot_start_lines.keys())[
-                    list(pot_start_lines.values()).index(True)
-                ]  # lines are one indexed
-            else:
-                logging.warning("multiple candidates for starting index")
-                # ToDo handle all the cases better again here. Smartest choice is that, for each candidate check match to the next line etc (recursively)
-                start_ind = list(pot_start_lines.keys())[
-                    list(pot_start_lines.values()).index(True)
-                ]
-            self.start_line_pre_edit = start_ind
-            # This should now be fulfilled by default
-            assert is_similar(self.lines[0][1], lines_dict[self.start_line_pre_edit])
+            if not self.find_start_line(lines_dict, problems):
+                return False
 
         # Now we should be able to validate the hunk line by line and add missing line
         hunk_ind = 0
