ed762ddb83405196bb2eb91829921ab4e88ed866
Theo McCabe <theomccabe@mac.com>
2024-03-09 16:44:06 +0000

    log exception

--- tests/caching_ai.py
+++ tests/caching_ai.py
@@ -8,6 +8,7 @@ from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
 from gpt_engineer.core.ai import AI
 from gpt_engineer.core.token_usage import TokenUsageLog
+from gpt_engineer.core.prompt import Prompt
 
 # Type hint for a chat message
 Message = Union[AIMessage, HumanMessage, SystemMessage]
@@ -22,17 +23,19 @@ class CachingAI(AI):
         try:
             self.model_name = "gpt-4-1106-preview"
             self.llm = self._create_chat_model()
-        except:  # Catch anything
+        except Exception as e:
+            print(f"Failed to create chat model: {e}")
             self.model_name = "cached_response_model"
             self.llm = None
         self.streaming = False
         self.token_usage_log = TokenUsageLog("gpt-4-1106-preview")
         self.cache_file = Path(__file__).parent / "ai_cache.json"
+        print(self.llm)
 
     def next(
         self,
         messages: List[Message],
-        prompt: Optional[str] = None,
+        prompt: Optional[str | Prompt] = None,
         *,
         step_name: str,
     ) -> List[Message]:
@@ -54,9 +57,10 @@ class CachingAI(AI):
         List[Message]
             The updated list of messages in the conversation.
         """
-
-        if prompt:
+        if  isinstance(prompt, str):
             messages.append(HumanMessage(content=prompt))
+        if  isinstance(prompt, Prompt):
+            messages.append(HumanMessage(content=prompt.to_langchain_content()))
 
         # read cache file if it exists
         if os.path.isfile(self.cache_file):
@@ -65,6 +69,7 @@ class CachingAI(AI):
         else:
             cache = dict()
 
+        print(self.llm)
         messages_key = self.serialize_messages(messages)
         if messages_key not in cache:
             print("calling backoff inference")
