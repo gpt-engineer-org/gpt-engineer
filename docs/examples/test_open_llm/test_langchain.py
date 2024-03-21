99e7dd9c21cd94e77a96b834900c4fc749704938
Ziga Brencic <ziga.brencic@gamil.com>
2024-03-21 18:51:59 +0100

    Ruff fixes

--- docs/examples/test_open_llm/test_langchain.py
+++ docs/examples/test_open_llm/test_langchain.py
@@ -1,13 +1,15 @@
-from langchain_openai import ChatOpenAI
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_openai import ChatOpenAI
 
 model = ChatOpenAI(
     model="CodeLlama",
     temperature=0.1,
     callbacks=[StreamingStdOutCallbackHandler()],
-    streaming=True
+    streaming=True,
 )
 
-prompt = "Provide me with only the code for a simple python function that sums two numbers."
+prompt = (
+    "Provide me with only the code for a simple python function that sums two numbers."
+)
 
 model.invoke(prompt)
--- docs/examples/test_open_llm/test_openai_api.py
+++ docs/examples/test_open_llm/test_openai_api.py
@@ -5,7 +5,10 @@ client = OpenAI(base_url="http://localhost:8000/v1", api_key="sk-xxx")
 response = client.chat.completions.create(
     model="CodeLlama",
     messages=[
-        {"role": "user", "content": "Provide me with the code for a simple HTML web site."},
+        {
+            "role": "user",
+            "content": "Provide me with the code for a simple HTML web site.",
+        },
     ],
     temperature=0.7,
     max_tokens=200,
