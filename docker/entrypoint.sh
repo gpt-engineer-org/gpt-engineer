d5a73ab3bdad7543f81b910ad23131d3a6877ba6
Plamen Ivanov <65338982+k1lgor@users.noreply.github.com>
2024-03-21 20:44:17 +0200

    Docker performance improvements and reduce image size (#1016)
    * refactor(makefile): :recycle: remove repetitive command
    
    * chore(Dockerfile): Refactor Dockerfile to use multi-stage build
    - Change base image from python:3.11-slim to python:3.11-alpine in the builder stage
    - Use apk package manager instead of apt-get to install dependencies
    - Remove unnecessary sudo command
    - Use pip install --no-cache-dir instead of pip install
    - Add second stage in Dockerfile for final image
    - Copy dependencies and entrypoint script from builder stage to final stage
    
    Signed-off-by: Plamen Ivanov <paco.iwanow@gmail.com>
    
    * chore(entrypoint.sh): update shebang to use `/usr/bin/env sh`
    - add coding utf-8 declaration
    - use `find` command to patch permissions of generated files
    
    Signed-off-by: Plamen Ivanov <paco.iwanow@gmail.com>
    
    ---------
    
    Signed-off-by: Plamen Ivanov <paco.iwanow@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+
 project_dir="/project"
 
 # Run the gpt engineer script
 gpt-engineer $project_dir "$@"
 
 # Patch the permissions of the generated files to be owned by nobody except prompt file
-for item in "$project_dir"/*; do
-    if [[ "$item" != "$project_dir/prompt" ]]; then
-        chown -R nobody:nogroup "$item"
-        chmod -R 777 "$item"
-    fi
-done
+find "$project_dir" -mindepth 1 -maxdepth 1 ! -path "$project_dir/prompt" -exec chown -R nobody:nogroup {} + -exec chmod -R 777 {} +
