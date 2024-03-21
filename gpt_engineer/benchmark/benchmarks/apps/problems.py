911e0921fa601d7a36c233fda174a1457f5edf5b
azrv <yellowbang368@gmail.com>
2024-03-21 19:00:24 +0200

    Clean up

--- gpt_engineer/benchmark/benchmarks/apps/problems.py
+++ gpt_engineer/benchmark/benchmarks/apps/problems.py
@@ -1,9 +1,3 @@
-INTRODUCTORY_PROBLEM_IDS = list(range(2500, 2600))
-
-INTERVIEW_PROBLEM_IDS = list(range(0, 100))
-
-COMPETITION_PROBLEM_IDS = list()
-
 # TODO: Pick problems
 # Temporary testing against these problems
 PROBLEM_IDS = list(range(0, 50))
