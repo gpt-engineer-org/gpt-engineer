025d5f303ee7d2b28156850310cde04ae862ef3b
Talion <yzheng10@unb.ca>
2024-03-31 13:50:49 -0300

    add projects/* exception for gitignore filter

--- gpt_engineer/applications/cli/file_selector.py
+++ gpt_engineer/applications/cli/file_selector.py
@@ -381,7 +381,7 @@ class FileSelector:
 
                 all_files.append(str(relpath))
 
-        if is_git_repo(project_path):
+        if is_git_repo(project_path) and "projects" not in project_path.parts:
             all_files = filter_by_gitignore(project_path, all_files)
 
         return all_files
